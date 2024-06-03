3c3bebdac6c8b028cdda9268ad6a6e370654898f
WMSLL <62074815+WMSLL@users.noreply.github.com>
2024-06-03 14:08:38 +0300

    Создание индекса

---
+++ WH_012/CtreateIndex_WH_012.sql
@@ -0,0 +1,28 @@
+-- Для получения не занятой ячейки выдачи
+Select top 1 l.Location
+		  From Warehouse.Location l left join  [Sales].[ShippingContainer] sc on sc.Location=l.Location
+		  where l.Location like 'DockDoor%'
+		  and sc.Location is null
+
+CREATE NONCLUSTERED INDEX [IX_LocationSc_20240602] ON [Sales].[ShippingContainer]
+(
+	[Location] ASC
+)
+
+
+
+-- запрос отображает Какой менеджер сколько сделал заказов на какое количество штук и количество SKU ,в разрезе определенного периода времени.
+-- для данного запроса мождно сделать такой индекс, но так как сейчас табицы с небольшим количество данных от такого инждекса толку мало,
+CREATE NONCLUSTERED INDEX [IX_DateTimeCreate_20240603] ON [Sales].ShipmentHeader
+(
+	DateTimeCreate ASC
+)
+
+
+Select up.Description,count(distinct sd.InternalShipmentLineNum) [Количество строк] ,sum(cast(sd.TotalQty as int)) [Количество шт всего] ,sum(cast(sd.QuantityOriginal as int)) [Изначальное количество шт]
+                     ,sum(cast(sd.TotalQty*Price as int))  [Стоимсоть]
+From Sales.ShipmentHeader sh join Application.UserProfile up on up.ObjectId=sh.UserId
+                             join Sales.ShipmentDetail sd on sd.InternalShipmentNum=sh.InternalShipmentNum
+where sh.DateTimeCreate between '2024-06-02' and '2024-06-03'
+
+Group by up.Description
