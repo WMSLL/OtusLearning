e04f410585d78f56c24f6685beafdf506b83235a
WMSLL <62074815+WMSLL@users.noreply.github.com>
2024-09-19 20:44:16 +0300

    Изменения в ДЗ 18

--- HW_018/Оптимизация запроса.sql
+++ HW_018/Оптимизация запроса.sql
@@ -1,37 +1,40 @@
 
 
-With ttt as (
+if  OBJECT_ID('tempdb..#sum')!=0 drop table #sum
 
-SELECT ord.CustomerID , det.StockItemID , det.UnitPrice , det.Quantity , ord.OrderID,sum(det.UnitPrice*det.Quantity) over(partition by inv.CustomerID ) sum_customer
-FROM Sales.Orders AS ord JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
-                         JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
-                         JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
-                         JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
-						 join Warehouse.StockItems it on It.StockItemID = det.StockItemID and it.SupplierId=12
+SELECT ordTotal.CustomerID
+into #sum
+FROM Sales.OrderLines AS Total
+Join Sales.Orders AS ordTotal
+On ordTotal.OrderID = Total.OrderID
+Group by ordTotal.CustomerID
+having SUM(Total.UnitPrice*Total.Quantity)>250000
+
+
+Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)
+FROM Sales.Orders AS ord
+          JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
+          JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
+       --   JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID -- из данной таблицы не тянем ни какие данные и по ней нет фильтров, зачем она в джойне ?!
+         -- JOIN Warehouse.StockItemTransactions AS ItemTrans   ON ItemTrans.StockItemID = det.StockItemID  -- из данной таблицы не тянем ни какие данные и по ней нет фильтров, зачем она в джойне ?!
+
+   join Warehouse.StockItems AS It on It.StockItemID=det.StockItemID and it.SupplierID=12
+    
+	join #sum s on s.CustomerID=Inv.CustomerID 
 WHERE Inv.BillToCustomerID != ord.CustomerID
+-- избавляемся от подзапросов
+--AND (Select SupplierId
+--FROM Warehouse.StockItems AS It
+--Where It.StockItemID = det.StockItemID) = 12
 
--- Если есть возможность убрать подзапросы , делаем это
-/*
-      AND
-      ( SELECT SupplierId
-        FROM Warehouse.StockItems AS It
-        WHERE It.StockItemID = det.StockItemID
-      ) = 12
-*/
-      AND
-      --( SELECT SUM(Total.UnitPrice * Total.Quantity)
-      --  FROM Sales.OrderLines AS Total JOIN Sales.Orders AS ordTotal ON ordTotal.OrderID = Total.OrderID
-      --  WHERE ordTotal.CustomerID = Inv.CustomerID
-      --) > 250000
-      --AND
-	   
-	   
-      DATEDIFF(dd , Inv.InvoiceDate , ord.OrderDate) = 0
---GROUP BY ord.CustomerID , det.StockItemID
-)
+-- Вынес данный подзапрос в отдельную таблдицу
+--AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+--FROM Sales.OrderLines AS Total
+--Join Sales.Orders AS ordTotal
+--On ordTotal.OrderID = Total.OrderID
+--WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
 
 
-Select CustomerID,StockItemID,sum(UnitPrice),sum(Quantity),count( OrderID)
-From ttt
-where sum_customer>250000
-Group by CustomerID,StockItemID
+AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID;
